c8eec87d15e963c99b71fad0c5ea55870980849e
Qunst <davorinkunst.1@gmail.com>
2020-06-02 18:25:09 +0200

    test collections 2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace list_tutorial
 {
     class Program
     {
-        static void Main(string[] args)
+        static void workingstings()
         {
             var names=new List<string> {"Dado", "Ana", "Filip"};
             foreach (var name in names)
@@ -20,7 +20,41 @@ namespace list_tutorial
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");             
             }
-                
+
+            Console.WriteLine();
+            Console.WriteLine($"Ja sam: {names[0]}");
+            Console.WriteLine($"Dodo sam: {names[2]} i {names[3]} na listu");
+            Console.WriteLine($"Lista ima {names.Count} imena");
+
+            var index = names.IndexOf("Filip");
+            if (index==-1)
+            {
+                Console.WriteLine($"Kad itema nema, IndexOf vraca {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Ime {names[index]} je na broju {index}");
+            }
+            index=names.IndexOf("Not Found");
+            if (index==-1)
+            {
+                Console.WriteLine($"Kad itema nema, IndexOf vraca {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Ime {names[index]} je na broju {index}");
+            }
+            Console.WriteLine();
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Bok {name.ToUpper()}!");
+            } 
+            
+        }
+        static void Main(string[] args)
+        {
+            //workingstings();
         }
     }
 }
